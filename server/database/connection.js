6661a1c58ff8c6d3e1e41f79490e5fcfa0112910
Ines Trabelsi <ines.jenfaoui@gmail.com>
2025-05-05 23:54:03 +0200

    add transaction to swagger file

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose')
-const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+const databaseUrl = 'mongodb://192.168.1.43:27017/argentBankDB'
 
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
+    await mongoose.connect(databaseUrl)
     console.log('Database successfully connected')
   } catch (error) {
     console.error(`Database Connectivity Error: ${error}`)
